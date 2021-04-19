c6d17c2e011da799e2c6bf9cfde60cb1c8d0b7b7
Mary Toth <maryarylynn@gmail.com>
2021-04-19 13:40:18 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BlackJackCS.Models;
+using System.Linq;
+
+namespace BlackJackCS
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
